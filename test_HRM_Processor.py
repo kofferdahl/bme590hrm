0bb4f2a7bded7fcc0b2e1ace2f1a3b71e5b1046e
Katelyn <katelyn.offerdahl@duke.edu>
2018-10-19 09:45:16 -0400

    Created conftest.py with basic pytest fixtures for generic DataReader and HRM_Processor objects that frequently get used in a variety of tests. Also modifies test_HRM_Processor.py to use the fixtures.

--- test_HRM_Processor.py
+++ test_HRM_Processor.py
@@ -3,9 +3,19 @@ from DataReader import DataReader
 from HRM_Processor import HRM_Processor
 
 
-def test_HRM_Processor_init():
-    dr = DataReader("test_file.csv")
+def test_HRM_Processor_init(dr):
+    """
 
+    Parameters
+    ----------
+    dr:     DataReader
+            A basic data reader object created from the file test_file.csv
+            with a default BPM duration.
+
+    Returns
+    -------
+
+    """
     hrm_proc = HRM_Processor(dr)
-
     assert dr.output_dict == hrm_proc.input_data
+
